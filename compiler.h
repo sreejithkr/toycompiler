00d15332021822c74645b0fdb9f8357bb0a475ef
Sreejith <>
2024-05-18 07:53:52 +0530

    Created the tocken structure

--- compiler.h
+++ compiler.h
@@ -2,6 +2,44 @@
 #define TOYCOMPILER_H
 
 #include <stdio.h>
+#include <stdbool.h>
+
+
+struct pos {
+    int line;
+    int col;
+    const char* filename;
+};
+
+enum {
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token {
+    int type;
+    int flags;
+
+    union  
+    {
+        char cval;
+        const char* sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+    // represents the state which say if there is whitespace between the current and next tocken.
+    bool whitespaces;
+    const char* between_brackets;
+
+};
 
 enum {
     COMPILER_FILE_COMPILED_OK,
